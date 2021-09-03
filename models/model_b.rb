372097f8263f19e4838acc2823506fd0e46a21ba
Cole Bradley <cole.bradley303@gmail.com>
2021-09-03 13:40:32 -0600

    Add model for developer

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+"job_title" => "Software Engineer",
+"school" => "Turing",
+"mean_age" => 30,
+"mean_salary" => 80000,
+"smart" => true,
+"good_looking" => true,
+"language_of_choice" => ["Ruby", "C++", "JS"],
+"info_sources" => ["Google", "Stackoverflow", "100min"],
+}
