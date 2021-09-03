7dd90aa4467049e96eba8f1a334f60628406c2f6
Cole Bradley <cole.bradley303@gmail.com>
2021-09-03 13:17:33 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,15 @@
+movie = "Gladiator"
+
+is_entertaining = true
+
+movie = "The Other Guys"
+
+num_4 = 10
+num_6 = 20
+
+product = num_4 * num_6
+
+favorite_foods = ["pizza", "pot roast", "chicken"]
+favorite_foods.delete_at(0)
+
+movie.length
