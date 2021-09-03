18c586422748088cb3d68bc0f1a0659405493a1e
Cole Bradley <cole.bradley303@gmail.com>
2021-09-03 13:31:21 -0600

    Add model for dinosaur

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+"name" => "T-Rex",
+"location" => "Earth",
+"age" => 2000,
+"alive" => 60,
+"furosious" => true,
+"massive" => true,
+"types_of_food" => ["people", "animals", "other dinosaur's"],
+"friends" => ["not humans", "sharks", "aligator"],
+}
